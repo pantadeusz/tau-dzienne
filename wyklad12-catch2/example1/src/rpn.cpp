8f6f41237eeb67ec4c8400095dbdb2c9efbd547c
pantadeusz <tadeusz@puzniakowski.pl>
2018-06-04 11:41:30 +0200

    przyklad wygenerowany na  wykladzie

--- wyklad12-catch2/example1/src/rpn.cpp
+++ wyklad12-catch2/example1/src/rpn.cpp
@@ -1,57 +1,73 @@
 #include "rpn.hpp"
 
-#include <map>
 #include <functional>
-#include <vector>
-#include <regex>
-#include <list>
 #include <iostream>
+#include <list>
+#include <map>
+#include <regex>
+#include <vector>
 
-double rpn_count(std::vector < std::string > & formula_tokens) {
-	
-	auto pop = [](std::list < double > &stack){
-		auto ret = stack.back();
-		stack.pop_back();
-		return ret;
-	};
-	
-	std::map < std::string, std::function < void(std::list < double >&) > > 
-		commands = {
-			{"+",[pop](std::list < double > &stack){
-				stack.push_back(pop(stack)+pop(stack));
-			}},
-			{"*",[pop](std::list < double > &stack){
-				stack.push_back(pop(stack)*pop(stack));
-			}},
-			{"/",[pop](std::list < double > &stack){
-				auto a = pop(stack), b = pop(stack);
-				stack.push_back(b/a);
-			}},
-			{"-",[pop](std::list < double > &stack){
-				auto a = pop(stack), b = pop(stack);
-				stack.push_back(b-a);
-			}}
-		};
-	
-	std::list < double > stack; // stos
-	for (auto &token : formula_tokens) {
-		if (commands.count(token)) {
-			commands[token](stack);
-		} else {
-			try {
-				stack.push_back(std::stod(token));
-			} catch (...) {
-				throw std::invalid_argument(std::string("unrecognized token ") + token);
-			}
-		}
-	}
-	return stack.back();
+using namespace std;
+
+void rpn::put_op(const std::string& name,
+    std::function<void(std::list<double>&)> f)
+{
+    ops[name] = f;
 }
 
-double rpn_count(const std::string &program) {
-	std::regex re(" ");
-    std::sregex_token_iterator first{program.begin(), program.end(), re, -1}, last;
-    std::vector<std::string> program_tokens {first, last};
-    return rpn_count(program_tokens);
+double rpn::count(const std::string& f)
+{
+    std::regex re(" ");
+    std::sregex_token_iterator first{ f.begin(), f.end(), re, -1 }, last;
+    std::vector<std::string> program_tokens{ first, last };
+    list<double> mystack;
+    for (string& t : program_tokens) {
+        try {
+            ops.at(t)(mystack);
+        } catch (...) {
+            try {
+                mystack.push_back(stod(t));
+            } catch (...) {
+                throw invalid_argument(string("bad token: ") + t);
+            }
+        }
+    }
+
+    return mystack.back();
 }
 
+rpn::rpn()
+{
+    ops["+"] = [](auto& s) {
+        auto a = s.back();
+        s.pop_back();
+        auto b = s.back();
+        s.pop_back();
+        auto c = a + b;
+        s.push_back(c);
+    };
+    ops["-"] = [](auto& s) {
+        auto a = s.back();
+        s.pop_back();
+        auto b = s.back();
+        s.pop_back();
+        auto c = b - a;
+        s.push_back(c);
+    };
+    ops["*"] = [](auto& s) {
+        auto a = s.back();
+        s.pop_back();
+        auto b = s.back();
+        s.pop_back();
+        auto c = a * b;
+        s.push_back(c);
+    };
+    ops["/"] = [](auto& s) {
+        auto a = s.back();
+        s.pop_back();
+        auto b = s.back();
+        s.pop_back();
+        auto c = b / a;
+        s.push_back(c);
+    };
+}
--- wyklad12-catch2/example1/tests/test_lib.cpp
+++ wyklad12-catch2/example1/tests/test_lib.cpp
@@ -2,23 +2,23 @@
 
 #include <rpn.hpp>
 
-using Catch::Matchers::EndsWith; // or Catch::EndsWith
+using Catch::Matchers::EndsWith; // see "matchers"
 
-TEST_CASE("Some test") {
-    std::string str = "testowanie aplikacji";
-    REQUIRE_THAT( str, EndsWith( "aplikacji" ) ); 
+TEST_CASE("Some test 2")
+{
+    REQUIRE((10 + 2) == 12);
 }
 
-TEST_CASE("Some test 2") {
-    REQUIRE ( rpn_count("10 2 +") == 12 );
-}
-
-SCENARIO ("Simple scenario") {
-    GIVEN ("there are numbers 10 and 20") {
-        int x= 10, y = 20;
-        WHEN ("we add them") {
+SCENARIO("Simple scenario")
+{
+    GIVEN("there are numbers 10 and 20")
+    {
+        int x = 10, y = 20;
+        WHEN("we add them")
+        {
             int z = x + y;
-            THEN("the result should be 30"){
+            THEN("the result should be 30")
+            {
                 REQUIRE(z == 30);
             }
         }
