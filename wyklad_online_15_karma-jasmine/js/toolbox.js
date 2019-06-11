bd5c82a56090e5b29fb96c6022b2105a8e29860f
pantadeusz <none@none.com>
2019-06-11 05:54:31 +0200

    toolbox

---
+++ wyklad_online_15_karma-jasmine/js/toolbox.js
@@ -0,0 +1,25 @@
+// js module
+exports.toolbox = function() {
+  // some sample function - this function will not be exported
+    let reverseSimple = function(x){
+      let ret = "";
+        for (let i = 0; i < x.length; i++)
+          ret = '' + x[i] + ret;
+        return ret;
+    };
+    
+    return {
+      reverse:function(x){
+        if (x.selector) {
+          document.querySelectorAll(x.selector).forEach(
+            (element) => {
+              element.innerHTML = reverseSimple(element.innerHTML);
+            }
+          );
+          return {};
+        } else {
+          return reverseSimple(x);
+        }
+      }
+    };
+  };
