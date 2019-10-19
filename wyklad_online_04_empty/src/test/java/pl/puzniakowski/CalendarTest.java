48a01103468dd078d81d2dfbd94741ea278439b3
pantadeusz <none@none.com>
2019-10-19 22:26:38 +0200

    start point

--- wyklad_online_04_empty/src/test/java/pl/puzniakowski/CalendarTest.java
+++ wyklad_online_04_empty/src/test/java/pl/puzniakowski/CalendarTest.java
@@ -1,6 +1,27 @@
 
 package pl.puzniakowski;
 
-public class CalendarTest {
+import static org.junit.Assert.assertNotNull;
 
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+@RunWith(JUnit4.class)
+public class CalendarTest {
+    @Mock
+    TimeSource timeSource;
+
+    @Before
+    public void initMocks() {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    public void timeSourceNotNull() {
+        assertNotNull(timeSource);
+    }
 }
